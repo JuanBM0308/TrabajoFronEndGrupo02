481f2b341fa8cb3c9edcb9946a9f33507c33d37f
Miguel Cuellar <mcuellar@opencitylife.com>
2025-03-29 22:30:03 -0500

    Se ajusta el registro de cuentas para usar el user por defecto

--- src/pages/registerpage/RegisterPage.js
+++ src/pages/registerpage/RegisterPage.js
@@ -1,75 +1,99 @@
 import "./registerpage.css";
 import "react-toastify/dist/ReactToastify.css";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { doc, setDoc } from "firebase/firestore";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
-import { auth, db } from "../../components/firebase/FireBase";
 import Footer from "../../components/layouts/footer/Footer";
 import Navbar from "../../components/layouts/navbar/Navbar";
 
 const RegisterPage = () => {
+  const [, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [fname, setFname] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [users, setUsers] = useState([]); // Estado para almacenar los usuarios
+
+  const navigate = useNavigate(); // Usa useNavigate
+
+  useEffect(() => {
+    if (users.length === 0) {
+      fetch("/users.json")
+        .then((response) => response.json())
+        .then((data) => setUsers(data))
+        .catch((error) => console.error("Error cargando usuarios:", error));
+    }
+  }, [users]);
 
   const handleRegister = async (e) => {
     e.preventDefault();
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      const user = auth.currentUser;
-      if (user) {
-        await setDoc(doc(db, "Users", user.uid), {
-          email: user.email,
-          name: fname,
-        });
-      }
-      toast.success("Usuario registrado", {
-        position: "top-center",
-      });
-    } catch (error) {
-      toast.error(error.message, {
+
+    if (password !== confirmPassword) {
+      toast.error("Las contraseñas no coinciden", {
         position: "bottom-center",
       });
+
+      return;
     }
+
+    const user = users.find(
+      (u) => u.email === email && u.password === password
+    );
+
+    if (user) {
+      toast.error("El usuario ya existe", {
+        position: "bottom-center",
+      });
+
+      return;
+    }
+
+    const currentUser = users.find((u) => u.role === "user");
+
+    localStorage.setItem("access_token", currentUser.access_token);
+    toast.success("Inicio de sesión correcto", { position: "top-center" });
+
+    navigate("/account");
   };
 
   return (
     <section>
       <Navbar />
+
       <div className="my-5 py-5">
         <div className="container text-center mt-3 pt-5">
           <h2 className="form-weight-bold">Registrarse</h2>
           <hr className="mx-auto" />
         </div>
+
         <div className="mx-auto container">
           <form onSubmit={handleRegister} id="register-form">
             <div className="form-group">
-              <label>Name</label>
+              <label>Nombre</label>
               <input
                 type="text"
                 className="form-control"
                 id="register-name"
                 name="name"
-                placeholder="Name"
-                onChange={(e) => setFname(e.target.value)}
+                placeholder="Nombre"
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
+
             <div className="form-group">
-              <label>Email</label>
+              <label>Correo electronico</label>
               <input
                 type="text"
                 className="form-control"
                 id="register-email"
                 name="Email"
-                placeholder="Email"
+                placeholder="Correo electronico"
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
+
             <div className="form-group">
               <label>Contraseña</label>
               <input
@@ -81,6 +105,7 @@ const RegisterPage = () => {
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
+
             <div className="form-group">
               <label>Confirmar Contraseña</label>
               <input
@@ -89,9 +114,10 @@ const RegisterPage = () => {
                 id="register-confirm-password"
                 name="confirmpassword"
                 placeholder="Confirmar Contraseña"
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => setConfirmPassword(e.target.value)}
               />
             </div>
+
             <div className="form-group">
               <input
                 type="submit"
@@ -100,6 +126,7 @@ const RegisterPage = () => {
                 value="Register"
               />
             </div>
+
             <div className="form-group">
               <Link id="register-url" className="btn" to="/signin">
                 ¿Ya tienes una cuenta? Ingresar
@@ -108,6 +135,7 @@ const RegisterPage = () => {
           </form>
         </div>
       </div>
+
       <Footer />
     </section>
   );
