f8cdc7f9ab197e6afded0fafd672f7a9ec026737
Sebasdlo <scarranza@poligran.edu.co>
2025-04-01 20:16:49 -0500

    modificacion del navbar-admin con la adicion de AccesToken

--- src/components/layouts/navbar-admin/Navbar-admin.js
+++ src/components/layouts/navbar-admin/Navbar-admin.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
@@ -7,6 +7,24 @@ import logo from "../../../assets/imgs/logo.jpg";
 
 const NavbarAdmin = () => {
   const location = useLocation();
+  const [accessToken, setAccessToken] = useState(localStorage.getItem("access_token"));
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setAccessToken(localStorage.getItem("access_token"));
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("access_token");
+    setAccessToken(null);
+  };
 
   return (
     <section className="navbar navbar-expand-lg navbar-light bg-white py-3 sticky-top">
@@ -20,10 +38,10 @@ const NavbarAdmin = () => {
         </div>
 
         <div className="container-divisor">
-          <NavLink to="/admin/sale-page" className="nav-item">
+          <NavLink to="/admin/dashboard-page" className="nav-item">
             <div
               className={`select-line-button ${
-                location.pathname === "/admin/sale-page" ? "active" : ""
+                location.pathname === "/admin/dashboard-page" ? "active" : ""
               }`}
             ></div>
             <button type="button" className="user-list-button btn">
@@ -32,7 +50,7 @@ const NavbarAdmin = () => {
                 width="16"
                 height="16"
                 fill="#6728a1"
-                class="bi bi-house-door-fill"
+                className="bi bi-house-door-fill"
                 viewBox="0 0 16 16"
               >
                 <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
@@ -58,7 +76,7 @@ const NavbarAdmin = () => {
                 width="16"
                 height="16"
                 fill="#6728a1"
-                class="bi bi-people-fill"
+                className="bi bi-people-fill"
                 viewBox="0 0 16 16"
               >
                 <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
@@ -67,28 +85,36 @@ const NavbarAdmin = () => {
             </button>
           </NavLink>
 
-          <NavLink to="/" className="nav-item">
-            <button type="button" className="user-list-button btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="#6728a1"
-                class="bi bi-box-arrow-right"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
-                />
-                <path
-                  fill-rule="evenodd"
-                  d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
-                />
-              </svg>
-              <span className="text-customer-list">Cerrar</span>
-            </button>
-          </NavLink>
+          {accessToken ? (
+            <NavLink to="/" className="nav-item" onClick={handleLogout}>
+              <button type="button" className="user-list-button btn">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="#6728a1"
+                  className="bi bi-box-arrow-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
+                  />
+                </svg>
+                <span className="text-customer-list">Cerrar sesión</span>
+              </button>
+            </NavLink>
+          ) : (
+            <NavLink to="/signin" className="nav-item">
+              <button type="button" className="user-list-button btn">
+                <span className="text-customer-list">Iniciar sesión</span>
+              </button>
+            </NavLink>
+          )}
         </div>
       </div>
     </section>
