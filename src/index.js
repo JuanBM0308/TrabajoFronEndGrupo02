d6b11dc063f81faa3bf65a20d19ce1e0b4e93ebe
NicoolDqek <136205493+NicoolDqek@users.noreply.github.com>
2025-04-08 22:40:49 -0500

    comentarios agregados a index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+// Importa el archivo de estilos principal para la aplicación
 import './index.css';
 
+// Importa la biblioteca principal de React
 import React from 'react';
+// Importa ReactDOM, que se utiliza para renderizar componentes en el DOM
 import ReactDOM from 'react-dom';
 
+// Importa el componente BrowserRouter que permite el manejo de rutas en la aplicación
 import { BrowserRouter } from 'react-router-dom';
 
+// Importa el componente principal de la aplicación
 import App from './App';
 
+// Crea una raíz para renderizar la aplicación dentro del elemento con id 'app' en el HTML
 const root = ReactDOM.createRoot(document.getElementById('app'))
 
+// Renderiza el componente App dentro del enrutador BrowserRouter
 root.render(
     <BrowserRouter>
-    <App/>
+        <App/>
     </BrowserRouter>
 );
-
