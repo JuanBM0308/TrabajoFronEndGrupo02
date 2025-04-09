0ba6fa91f48890dd46e878f802621480c284a2bc
NicoolDqek <136205493+NicoolDqek@users.noreply.github.com>
2025-04-08 22:40:06 -0500

    comentarios agregados a app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import "react-toastify/dist/ReactToastify.css"; // Importar CSS aquí también
+import "react-toastify/dist/ReactToastify.css"; // Importar los estilos de Toastify para las notificaciones
 
-import React from "react";
+import React from "react"; // Importar React
 
+// Importar componentes necesarios para el enrutamiento
 import { Route, Routes } from "react-router-dom";
+// Importar el contenedor de notificaciones de Toastify
 import { ToastContainer } from "react-toastify";
 
+// Importar todas las páginas que forman parte de la aplicación
 import CartPage from "../src/pages/cartpage/CartPage.js";
 import AccountPage from "./pages/accountpage/AccountPage.js";
 import EditAccountPage from "./pages/editaccountpage/EditAccountPage.js";
@@ -22,41 +25,61 @@ import Clientlist from "./pages/admin/clientlist/Clientlist.js";
 import ClientPage from "./pages/admin/clientpage/ClientPage.js";
 import SalePage from "./pages/admin/salepage/SalePage.js";
 
+// Componente principal de la aplicación
 const App = () => {
   return (
     <div>
+      {/* Definición de todas las rutas de la aplicación */}
       <Routes>
+        {/* Ruta principal que carga la página de inicio */}
         <Route path="/" element={<HomePage />} />
+        {/* Página de productos */}
         <Route path="/products" element={<ProductsPage />} />
+        {/* Página de detalles de un producto, usa parámetro dinámico */}
         <Route path="/products-details/:id" element={<ProductsDetailsPage />} />
+        {/* Página de inicio de sesión */}
         <Route path="/signin" element={<SignInPage />} />
+        {/* Página del carrito de compras */}
         <Route path="/cart" element={<CartPage />} />
+        {/* Página de registro */}
         <Route path="/register" element={<RegisterPage />} />
+        {/* Página de cuenta del usuario */}
         <Route path="/account" element={<AccountPage />} />
+        {/* Página para editar cuenta */}
         <Route path="/edit-account" element={<EditAccountPage />} />
 
+        {/* Página para cambiar contraseña */}
         <Route
           path="/change-password-account"
           element={<ChangePasswordAccountPage />}
         />
 
+        {/* Página de contacto */}
         <Route path="/contact" element={<ContactPage />} />
+        {/* Página de búsqueda */}
         <Route path="/search" element={<SearchPage />} />
+        {/* Página de panel de control del admin */}
         <Route path="/admin/dashboard-page" element={<DashboardPage />} />
 
+        {/* Página con información del cliente (admin) */}
         <Route
           path="/admin/client-information-page"
           element={<ClientInformationPage />}
         />
 
+        {/* Lista de clientes (admin) */}
         <Route path="/admin/client-list" element={<Clientlist />} />
 
+        {/* Página individual de cliente (admin) */}
         <Route path="/admin/client-page" element={<ClientPage />} />
+        {/* Página de ventas (admin) */}
         <Route path="/admin/sale-page" element={<SalePage />} />
       </Routes>
+
+      {/* Contenedor para mostrar las notificaciones tipo Toast */}
       <ToastContainer />
     </div>
   );
 };
 
-export default App;
+export default App; // Exporta el componente App como módulo principal
